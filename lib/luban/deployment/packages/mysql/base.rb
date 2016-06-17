ce9e32cd0c955dfcd844d6dbc6767bb097705ee7
Chi Man Lei <chi.lei@laxino.com>
2016-06-17 10:27:59 +0800

    Corrected the package major version

--- lib/luban/deployment/packages/mysql/base.rb
+++ lib/luban/deployment/packages/mysql/base.rb
@@ -2,17 +2,17 @@ module Luban
   module Deployment
     module Packages
       class Mysql < Luban::Deployment::Service::Base
+        def self.decompose_version(version)
+          vers = version.split('.')
+          { major_version: "#{vers[0]}.#{vers[1]}", patch_level: vers[2] }
+        end
+
         def default_templates_path
           @default_templates_path ||= Pathname.new(File.join(File.dirname(__FILE__), 'templates')).realpath
         end
 
         %i(reload_process reopen_logs ping_process).each do |action|
           service_action action, dispatch_to: :controller
-        end
-
-        def decompose_version(version)
-          vers = version.split('.')
-          { major_version: "#{vers[0]}.#{vers[1]}", patch_level: vers[2] }
         end
 
         protected
